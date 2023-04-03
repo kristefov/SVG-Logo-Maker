f398d6e7d4f9303287513d345a9d9f705e9d8ce3
kristiyan <kristiyan.tefov@icloud.com>
2023-04-03 18:06:45 +0100

    Validate

--- index.js
+++ index.js
@@ -1,27 +1,69 @@
 const inqurier = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
+const isColor = require('is-color')
+const { Circle, Triangle, Square } = require('./lib/shapes');
 
 const questions = [
   { type: 'input',
-    name: 'logoText',
-    message: 'Enter text for the logo (Must be more than 1 character)'
+    name: 'text',
+    message: 'Enter text for the logo (Up to 3 character)',
+    validate: text => {
+      if(text.length < 1 || text.length > 3){
+        return 'Invalid length'
+      }
+      return true
+    }
   },
   { type: 'input',
-    name: 'logoColor',
-    message: 'Enter a text color'
-  },
-  { type: 'input',
-    name: 'logoShape',
-    message: 'Enter shape for the logo'
+    name: 'color',
+    message: 'Enter a text color',
+    validate: color => {
+      if(!isColor(color)){
+        return 'Invalid color'
+      }
+      return true
+    }
   },
   {
     type: 'list',
     message: 'Choose shape for the logo',
-    name: 'license',
-    choices: ['Circle', 'Polygon', 'Eclipse','Rectangle' ]
+    name: 'shape',
+    choices: ['Circle', 'Triangle', 'Square']
   },
   { type: 'input',
-    name: 'logoShapeColor',
-    message: 'Enter a shape color'
+    name: 'shapeColor',
+    message: 'Enter a shape color',
+    validate: shapeColor => {
+      if(!isColor(shapeColor)){
+        return 'Invalid color'
+      }
+      return true
+    }
   },
 ];
+
+inqurier.prompt(questions).then((answers) => {
+  const svg = generateSVG(answers)
+  console.log(svg)
+  }
+)
+
+const generateSVG = ({color,text,shape,shapeColor}) => {
+  return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+  ${generateShapeSVG(shape, shapeColor)}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>
+  
+  </svg>`
+}
+
+const generateShapeSVG = (shape, shapeColor) => {
+  switch(shape){
+      case 'Circle':
+        return new Circle(shapeColor)
+      case 'Triangle':
+        return new Triangle(shapeColor)
+      case 'Square':
+        return new Square(shapeColor)
+      default:
+  }
+}
