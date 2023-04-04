1d60558914ee93fbf1e23ea432a0b5e6d800e3c5
kristiyan <kristiyan.tefov@icloud.com>
2023-04-04 23:16:54 +0100

    Comments

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const inqurier = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
+// npm that check if the color is valid
 const isColor = require('is-color')
+// importing the objects from shapes.js
 const { Circle, Triangle, Square } = require('./lib/shapes');
-
+// questions for user to generate a logo based on his/her answers
 const questions = [
   { type: 'input',
     name: 'text',
     message: 'Enter text for the logo (Up to 3 character)',
+    // validate user input
     validate: text => {
       if(text.length < 1 || text.length > 3){
         return 'Invalid length'
@@ -17,6 +20,7 @@ const questions = [
   { type: 'input',
     name: 'color',
     message: 'Enter a text color',
+    // validate user input
     validate: color => {
       if(!isColor(color)){
         return 'Invalid color'
@@ -33,6 +37,7 @@ const questions = [
   { type: 'input',
     name: 'shapeColor',
     message: 'Enter a shape color',
+    // validate user input
     validate: shapeColor => {
       if(!isColor(shapeColor)){
         return 'Invalid color'
@@ -41,21 +46,20 @@ const questions = [
     }
   },
 ];
-
-inqurier.prompt(questions).then((answers) => {
-  const svg = generateSVG(answers)
-  console.log(svg)
-  }
-)
-
+// function to create the logo.svg file in logo directory
+inquirer.prompt(questions).then((answers) => {
+  fs.writeFile('./logo/logo.svg', generateSVG(answers), (err) => {
+    err ? console.log(err) : console.log("Successfully created", "\x1b[32m Generated logo.svg")
+  })})
+// function to generate the SVG Logo
 const generateSVG = ({color,text,shape,shapeColor}) => {
   return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
   ${generateShapeSVG(shape, shapeColor)}
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>
-  
   </svg>`
 }
 
+// function to generate the shape of the SVG
 const generateShapeSVG = (shape, shapeColor) => {
   switch(shape){
       case 'Circle':
